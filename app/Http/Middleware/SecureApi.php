7024dc4fc79cc19703fcbef55984c58799ddc1ec
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-23 17:06:25 +0000

    feat: add jwt and admin check middleware

---
+++ app/Http/Middleware/SecureApi.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Dtos\User;
+use App\Services\Interfaces\JwtTokenProviderInterface;
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecureApi
+{
+    private User $user;
+    private readonly JwtTokenProviderInterface $jwt_service;
+
+    public function __construct()
+    {
+        $this->jwt_service = app(JwtTokenProviderInterface::class);
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @param string $guard
+     * @return Response
+     * @throws AuthenticationException
+     */
+    public function handle(Request $request, Closure $next, string $guard = ''): Response
+    {
+        $this->handleAuthentication($request);
+
+        return $this->handleAuthorization($request, $next, $guard);
+    }
+
+    /**
+     * @throws AuthenticationException
+     */
+    private function handleAuthentication(Request $request): void
+    {
+        if (empty($request->bearerToken())) {
+            throw new AuthenticationException();
+        }
+        $user = $this->jwt_service->authenticate($request->bearerToken());
+        if (empty($user)) {
+            throw new AuthenticationException('invalid bearer token');
+        }
+        $this->user = $user;
+    }
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @param string $guard
+     * @return Response
+     */
+    private function handleAuthorization(Request $request, Closure $next, string $guard): Response
+    {
+        if ($guard === 'admin') {
+            if ($this->user->is_admin) {
+                return $next($request);
+            } else {
+                throw new UnauthorizedException('not authorized to access admin');
+            }
+        }
+        return $next($request);
+    }
+}
