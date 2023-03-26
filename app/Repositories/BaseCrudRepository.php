ef3f13515876fc81e1ae2b2d5febcdf1b73cd61b
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-26 02:56:01 +0000

    feat: brands feature implementation     - implement base crud repository     - implement brand repository as subclass

---
+++ app/Repositories/BaseCrudRepository.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Dtos\BaseDto;
+use App\Repositories\Interfaces\CrudRepositoryInterface;
+use App\Repositories\Traits\SupportsPagination;
+use App\Repositories\Traits\SupportsPaginationTraitInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use ReflectionClass;
+use ReflectionException;
+
+/**
+ * @template TModel of Model
+ * @template TObj of BaseDto
+ * @implements CrudRepositoryInterface<TModel, TObj>
+ * @implements SupportsPaginationTraitInterface<Model|TModel>
+ */
+abstract class BaseCrudRepository implements CrudRepositoryInterface, SupportsPaginationTraitInterface
+{
+    use SupportsPagination;
+
+    protected BaseDto $dto;
+    protected Model $model;
+    private string $modelClass;
+
+    /**
+     * @throws ReflectionException
+     */
+    public function __construct(?string $modelClass = null, ?string $dtoClass = null)
+    {
+        $this->modelClass = $modelClass ?: self::guessModelClass();
+        $this->model = app($this->modelClass);
+
+        /** @var class-string<TObj> $dto_class */
+        $dto_class = $dtoClass ?: self::guessDtoClass();
+        $r = new ReflectionClass($dto_class);
+        $this->dto = $r->newInstanceWithoutConstructor();
+    }
+
+    private static function guessModelClass(): string
+    {
+        return self::guessClass('Models');
+    }
+
+    private static function guessClass(string $location): string
+    {
+        return preg_replace(
+            '/(.+)\\\\Repositories\\\\(.+)Repository$/m',
+            '$1\\' . $location . '\\\$2',
+            static::class
+        ) ?: '';
+    }
+
+    private static function guessDtoClass(): string
+    {
+        return self::guessClass('Dtos');
+    }
+
+    public function create(array $data)
+    {
+        /** @var TModel|null $model */
+        $model = $this->model::query()->create($data);
+        return $model ? $this->dto::make($model->getAttributes()) : null;
+    }
+
+    /**
+     * @return LengthAwarePaginator<TModel|Model>
+     */
+    public function getList(): LengthAwarePaginator
+    {
+        $query = $this->model::query();
+
+        return $this->withPaginate($query);
+    }
+
+    public function deleteByUuid(string $uuid): bool
+    {
+        return $this->forUserByUuid($uuid)->delete();
+    }
+
+    /**
+     * @param string $uuid
+     * @return Builder<TModel>
+     */
+    private function forUserByUuid(string $uuid): Builder
+    {
+        return $this->model::query()->where('uuid', $uuid);
+    }
+
+    public function findByUuid(string $uuid)
+    {
+        $model = $this->forUserByUuid($uuid)->first();
+        return $model ? $this->dto::make($model->getAttributes()) : null;
+    }
+
+    public function updateByUuid(string $uuid, array $data)
+    {
+        /** @var TModel|null $model */
+        $model = $this->forUserByUuid($uuid)->first();
+        $updated = $model?->update($data);
+        return $updated ? $this->dto::make($model->getAttributes()) : null;
+    }
+}
---
+++ app/Repositories/BrandRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Dtos\Brand as BrandDto;
+use App\Models\Brand;
+
+/**
+ * @extends BaseCrudRepository<Brand, BrandDto>
+ */
+class BrandRepository extends BaseCrudRepository
+{
+}
