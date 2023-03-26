edc51b4b98b15395bde76d2ec9eb2b6bc13683aa
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-26 02:55:13 +0000

    feat: brands feature definition     - add interfaces

---
+++ app/Repositories/Interfaces/CrudRepositoryInterface.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Dtos\BaseDto;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @template TModel of Model
+ * @template TObj of BaseDto
+ */
+interface CrudRepositoryInterface
+{
+    /**
+     * Create a model
+     *
+     * @param array<string, mixed> $data
+     * @return TObj|BaseDto|null
+     */
+    public function create(array $data);
+
+    /**
+     * Get list paginated list of model
+     *
+     * @return LengthAwarePaginator<TModel>
+     */
+    public function getList(): LengthAwarePaginator;
+
+    /**
+     * Delete model by uuid
+     *
+     * @param string $uuid
+     * @return bool
+     */
+    public function deleteByUuid(string $uuid): bool;
+
+    /**
+     * Find a model by uuid
+     *
+     * @param string $uuid
+     * @return TObj|BaseDto|null
+     */
+    public function findByUuid(string $uuid);
+
+    /**
+     * Update by uuid
+     *
+     * @param string $uuid
+     * @param array<string, mixed> $data
+     * @return TObj|BaseDto|null
+     */
+    public function updateByUuid(string $uuid, array $data);
+}
