373e3d4f338da2f1a30d6182a513cc34657374ad
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-27 21:58:28 +0000

    chore: add support for json relations
        - update product migration to set brand relation in json field

--- database/migrations/2023_03_18_013906_create_products_table.php
+++ database/migrations/2023_03_18_013906_create_products_table.php
@@ -10,17 +10,31 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        $driver = Schema::getConnection()->getDriverName();
+        Schema::create('products', function (Blueprint $table) use ($driver) {
             $table->bigIncrements('id');
-            $table->foreignUuid('category_uuid')->constrained('categories', 'uuid');
+            $table->foreignUuid('category_uuid')->constrained('categories', 'uuid')->cascadeOnDelete();
             $table->string('uuid')->unique();
             $table->string('title');
             $table->float('price', 12);
             $table->text('description');
             $table->json('metadata');
+
+            if ($driver == 'mysql') {
+                $brand = DB::connection()->getQueryGrammar()->wrap('metadata->brand');
+                $table->uuid('brand_uuid')->storedAs($brand);
+                $table->foreign('brand_uuid')->references('uuid')->on('brands')->cascadeOnDelete();
+            }
+
+            if ($driver == 'sqlsrv') {
+                $brand = DB::connection()->getQueryGrammar()->wrap('metadata->brand');
+                $brand_uuid = 'CAST(' . $brand . ' AS VARCHAR)';
+                $table->computed('brand_uuid', $brand_uuid)->persisted();
+                $table->foreign('brand_uuid')->references('uuid')->on('brands')->cascadeOnDelete();
+            }
+
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrentOnUpdate();
-            $table->softDeletes();
         });
     }
 
