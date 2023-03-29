9fb9571972b82f8bdd4b947fdfdb298c1f627ddd
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-29 17:23:39 +0000

    feat: add orders feature
        - update address value object with swagger

--- app/Values/Address.php
+++ app/Values/Address.php
@@ -2,6 +2,25 @@
 
 namespace App\Values;
 
+/**
+ *
+ * @OA\Schema(
+ *    schema="Address",
+ *    description="Billing and Shipping address",
+ *    required={"shipping", "billing"},
+ *    @OA\Property(
+ *     property="shipping",
+ *     type="string",
+ *     description="Shipping address",
+ *    ),
+ *     @OA\Property(
+ *     property="billing",
+ *     type="string",
+ *     description="Billing address",
+ *    )
+ * )
+ */
+
 final class Address extends BaseValueObject
 {
     public function __construct(
