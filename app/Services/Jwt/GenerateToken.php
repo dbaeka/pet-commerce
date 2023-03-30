7c9f97fb0d42b1f5016bd8f5f1f25625bd6fef97
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-30 14:43:51 +0000

    refactor: improve readability for Handler.php, AuthService.php, JwtService.php

---
+++ app/Services/Jwt/GenerateToken.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\Jwt;
+
+use App\Dtos\Token;
+use App\Dtos\User;
+
+class GenerateToken extends BaseJwtProvider
+{
+    public function execute(User $user): Token
+    {
+        $config = $this->config;
+
+        $now = now();
+        $expires_at = now()->addSeconds($this->expiry_seconds);
+
+        $unique_id = hash('sha256', strval(now()->timestamp));
+
+        $jwt = $config->builder()
+            ->issuedBy($this->issuer)
+            ->identifiedBy($unique_id)
+            ->withClaim('user_uuid', $user->uuid)
+            ->issuedAt($now->toDateTimeImmutable())
+            ->expiresAt($expires_at->toDateTimeImmutable())
+            ->getToken($config->signer(), $config->signingKey());
+
+        $data = [
+            'user_uuid' => $user->uuid,
+            'unique_id' => $unique_id,
+            'token_title' => 'Access Token',
+            'expires_at' => $expires_at,
+        ];
+        return Token::make($data)->withToken($jwt->toString());
+    }
+}
